e6da52dd9e6a83fd8bf2bd29e6d18363e91dbbce
unknown <gmzulkar@gmail.com>
2022-07-15 20:57:19 +0600

    pagination and data summary created

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -19,7 +19,7 @@ class ProductList(generic.ListView):
     template_name='products/list.html'
     model = product_model.Product
     context_object_name = 'products'
-    paginate_by = 10  # if pagination is desired
+    paginate_by = 4  # if pagination is desired
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
